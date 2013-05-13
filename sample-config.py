4349b8b413560b0e0b6c3d003d4acbc866732276
Kavin Yao <kavinyao@gmail.com>
2013-05-13 23:33:00 +0800

    update sample-config.py

--- sample-config.py
+++ sample-config.py
@@ -13,6 +13,27 @@ DB_CHARSET = 'utf8'
 # Tables
 TABLES = {
     'QUERY': 'query',
-    'ITEM': 'item',
-    'QUERY_ITEM': 'query_item',
+    'ITEM': 'product',
+    'QUERY_ITEM': 'query_product',
 }
+
+# Test config
+from splitter import KFoldSplitter
+from database import DatabaseManager
+from word_segment import SpaceWordSegmenter
+from evaluator import ConfusionMatrixEvaluator
+from recommender import KeywordRecommender, HottestRecommender, BCIPFMeasure
+
+# Top-N
+N = 5
+# for KFoldSplitter
+K = 10
+# Variations
+Splitter = KFoldSplitter
+WordSegmenter = SpaceWordSegmenter
+RelevanceMeasure = BCIPFMeasure
+Evaluator = ConfusionMatrixEvaluator
+Recommenders = (KeywordRecommender, HottestRecommender)
+
+# Misc
+stopwords = 'stopwords'
