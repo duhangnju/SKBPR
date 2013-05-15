25d0ffde153cb4f3aba60d0e73ff106013671731
Kavin Yao <kavinyao@gmail.com>
2013-05-15 15:35:26 +0800

    add necessary index

--- rec_tables.sql
+++ rec_tables.sql
@@ -4,22 +4,23 @@ DROP TABLE IF EXISTS keyword_product_weight;
 DROP TABLE IF EXISTS keyword;
 
 CREATE TABLE IF NOT EXISTS keyword_query(
-	id BIGINT(10) PRIMARY KEY AUTO_INCREMENT,
-	keyword VARCHAR(255),
-	query_id BIGINT(10),
+    id BIGINT(10) PRIMARY KEY AUTO_INCREMENT,
+    keyword VARCHAR(255),
+    query_id BIGINT(10),
     UNIQUE (keyword, query_id)
 );
 
 CREATE TABLE IF NOT EXISTS keyword(
-	keyword VARCHAR(255) PRIMARY KEY,
-	count BIGINT(10)
+    keyword VARCHAR(255) PRIMARY KEY,
+    count BIGINT(10)
 );
 
 CREATE TABLE IF NOT EXISTS keyword_product_weight(
-	id BIGINT(10) PRIMARY KEY AUTO_INCREMENT,
-	keyword VARCHAR(255) NOT NULL,
-	product VARCHAR(255) NOT NULL,
-	weight FLOAT(10)
+    id BIGINT(10) PRIMARY KEY AUTO_INCREMENT,
+    keyword VARCHAR(255) NOT NULL,
+    product VARCHAR(255) NOT NULL,
+    weight FLOAT(10),
+    KEY (keyword)
 );
 
 COMMIT;
--- tables.sql
+++ tables.sql
@@ -5,22 +5,25 @@ DROP TABLE IF EXISTS query;
 DROP TABLE IF EXISTS product;
 
 CREATE TABLE IF NOT EXISTS query (
-	id BIGINT(10) PRIMARY KEY AUTO_INCREMENT,
-	user_id VARCHAR(255) NOT NULL,
-	query VARCHAR(255) NOT NULL,
-	group_id SMALLINT NOT NULL DEFAULT 0
+    id BIGINT(10) PRIMARY KEY AUTO_INCREMENT,
+    user_id VARCHAR(255) NOT NULL,
+    query VARCHAR(255) NOT NULL,
+    group_id SMALLINT NOT NULL DEFAULT 0,
+    KEY (group_id)
 );
 
 CREATE TABLE IF NOT EXISTS product (
-	name VARCHAR(255) PRIMARY KEY
+    name VARCHAR(255) PRIMARY KEY
 );
 
 CREATE TABLE IF NOT EXISTS query_product (
-	id BIGINT(10) PRIMARY KEY AUTO_INCREMENT,
-	query_id BIGINT(10),
-	product_name VARCHAR(255),
-	bought INT(5), -- 0 stands for visit 1 stands for add to shopcart 2 stands for buy
-    sequence INT
+    id BIGINT(10) PRIMARY KEY AUTO_INCREMENT,
+    query_id BIGINT(10),
+    product_name VARCHAR(255),
+    bought INT(5), -- 0 stands for visit 1 stands for add to shopcart 2 stands for buy
+    sequence INT,
+    KEY (query_id),
+    KEY (product_name)
 );
 
 COMMIT;
