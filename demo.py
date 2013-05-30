305aeb846ee89234d8eae3b73452c2fdad2496b4
Kavin Yao <kavinyao@gmail.com>
2013-05-30 23:51:57 +0800

    update demo

--- demo.py
+++ demo.py
@@ -4,6 +4,8 @@ from database import DatabaseManager
 from recommender import KeywordRecommender
 from bottle import Bottle, run, request, view, static_file
 
+N = 5
+
 class get_dbm(object):
     def __enter__(self):
         self.dbm = DatabaseManager(config.DB_HOST, config.DB_USER, config.DB_PASSWORD, config.DB_NAME)
@@ -43,20 +45,21 @@ def home():
 def update_suggestions():
     query = request.POST.query
     with get_dbm() as dbm:
-        recommender = KeywordRecommender(config.N, dbm, config.WordSegmenter(), None)
+        recommender = KeywordRecommender(dbm, config.WordSegmenter(), None)
+        recommender.set_limit(N)
         products = [rec[0] for rec in recommender.recommend(query)]
 
         # make sure sufficient number of products is recommended
         rec_num = len(products)
-        if rec_num < config.N:
-            products.extend(get_hot_products(dbm)[:config.N-rec_num])
+        if rec_num < N:
+            products.extend(get_hot_products(dbm)[:N-rec_num])
 
         products = transfrom_results(products)
     return {'products': products}
 
 def get_hot_products(dbm):
     # source hot_cache.sql to make things go smoothly
-    hot_product_query = 'SELECT product FROM hot_cache ORDER BY weight DESC LIMIT %d' % config.N
+    hot_product_query = 'SELECT product FROM hot_cache ORDER BY weight DESC LIMIT %d' % N
     products = [row['product'] for row in dbm.get_rows(hot_product_query)]
     return products
 
